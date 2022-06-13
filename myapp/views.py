e428790311b36c2fee9883d08ad8b409d1b63980
mrifaz <mahmud12@uwindsor.ca>
2022-06-13 14:49:12 -0400

    lab 6 files

--- myapp/views.py
+++ myapp/views.py
@@ -16,15 +16,15 @@ def write_result(request):
 def index(request):
     top_list = Topic.objects.all().order_by('id')[:10]
     course_list = Course.objects.all().order_by('-price')[:5]
-    return render(request, 'myapp/index0.html', {'top_list': top_list, 'course_list': course_list})
+    return render(request, 'myapp/index.html', {'top_list': top_list, 'course_list': course_list})
 
 
 def about(request):
-    return render(request, 'myapp/about0.html')
+    return render(request, 'myapp/about.html')
 
 def  detail(request, top_no):
     #topic=get_object_or_404(Topic, pk=top_no)
     topic= Topic.objects.get(id=top_no)
     #get_object_or_404(topic, pk=1)
     course_list = Course.objects.filter(topic=Topic.objects.get(id=top_no))            
-    return render(request, 'myapp/detail0.html', {'topic': topic, 'course_list': course_list})
+    return render(request, 'myapp/detail.html', {'topic': topic, 'course_list': course_list})
